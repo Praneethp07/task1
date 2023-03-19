caa99627f76ea10647bf93b729621bcbb4cde291
praneeth <praneethp.cs20@sahyadri.edu.in>
2023-03-19 19:48:53 +0530

    final commit

--- task1_app/lib/main.dart
+++ task1_app/lib/main.dart
@@ -1,11 +1,16 @@
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'items.dart';
+import 'details/userdetails.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -14,6 +19,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: 'futura',
       ),
       home: const MyHomePage(),
       debugShowCheckedModeBanner: false,
@@ -22,69 +28,192 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var numOfPeopleAdded = 12;
+  @override
+  void initState() {
+    super.initState();
+    newUserList = user;
+  }
+
+  List<Users> newUserList = [];
+  List<Users> user = [
+    Users(
+        name: "Prof. Rama devananda Gopalji",
+        imgurl:
+            "https://images.unsplash.com/photo-1679198315253-88c0b0b3f4e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"),
+    Users(
+        name: "Alexa Mohammed",
+        imgurl:
+            "https://images.unsplash.com/photo-1679198315253-88c0b0b3f4e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"),
+    Users(
+        name: "Christopher Nolan",
+        imgurl:
+            "https://images.unsplash.com/photo-1679198315253-88c0b0b3f4e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"),
+    Users(
+        name: "CJ Magellan",
+        imgurl:
+            "https://images.unsplash.com/photo-1679198315253-88c0b0b3f4e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"),
+    Users(
+        name: "Clinton Baptist",
+        imgurl:
+            "https://images.unsplash.com/photo-1679198315253-88c0b0b3f4e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"),
+    Users(
+        name: "Dagis patekar",
+        imgurl:
+            "https://images.unsplash.com/photo-1679198315253-88c0b0b3f4e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"),
+    Users(
+        name: "Ferdinand Magellan",
+        imgurl:
+            "https://images.unsplash.com/photo-1679198315253-88c0b0b3f4e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"),
+    Users(
+        name: "Gangu Barman Shajahan",
+        imgurl:
+            "https://images.unsplash.com/photo-1679198315253-88c0b0b3f4e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"),
+    Users(
+        name: "Nirmal George Mathew",
+        imgurl:
+            "https://images.unsplash.com/photo-1679198315253-88c0b0b3f4e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"),
+    Users(
+        name: "Shreyas C",
+        imgurl:
+            "https://images.unsplash.com/photo-1679198315253-88c0b0b3f4e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"),
+    Users(
+        name: "Nirmal George Mathew",
+        imgurl:
+            "https://images.unsplash.com/photo-1679198315253-88c0b0b3f4e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"),
+    Users(
+        name: "Alexa Mohammed",
+        imgurl:
+            "https://images.unsplash.com/photo-1679198315253-88c0b0b3f4e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"),
+    Users(
+        name: "CJ Magellan",
+        imgurl:
+            "https://images.unsplash.com/photo-1679198315253-88c0b0b3f4e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"),
+    Users(
+        name: "Dagis patekar",
+        imgurl:
+            "https://images.unsplash.com/photo-1679198315253-88c0b0b3f4e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"),
+  ];
+  var numOfPeopleAdded = 0;
   @override
   Widget build(BuildContext context) => Scaffold(
         body: Container(
-          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+          color: Colors.white,
+          padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
-            // mainAxisSize: MainAxisSize.values.last,
             children: [
               const SizedBox(
-                height: 65,
+                height: 40.0,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
+                children: <Widget>[
+                  const Text(
                     "Add people",
-                    style: TextStyle(fontSize: 30),
+                    style: TextStyle(fontSize: 24.64),
                   ),
                   Text(
                     "$numOfPeopleAdded added",
-                    style: TextStyle(fontSize: 18),
+                    style: const TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ],
               ),
               const SizedBox(
-                height: 50,
+                height: 50.0,
               ),
               Container(
-                height: 60,
-                // margin: EdgeInsets.symmetric(horizontal: 20),
-                padding: EdgeInsets.symmetric(horizontal: 5),
+                height: 48.0,
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                alignment: Alignment.center,
                 decoration: BoxDecoration(
                     border: Border.all(),
                     color: Colors.white,
-                    borderRadius: BorderRadius.circular(10)),
+                    borderRadius: BorderRadius.circular(10.0)),
                 child: TextField(
-                  
-                  textAlign: TextAlign.center,
-                  textAlignVertical: TextAlignVertical.center,
+                  onChanged: (value) {
+                    if (value == "") {
+                      newUserList = user;
+                    } else {
+                      newUserList = [];
+                      user.forEach((element) {
+                        if (element.name.toLowerCase().startsWith(value) || element.name.startsWith(value)) {
+                          newUserList.add(element);
+                        }
+                      });
+                    }
+                    setState(() {});
+                  },
+                  style: TextStyle(fontSize: 16.0),
                   decoration: InputDecoration(
-                      contentPadding: EdgeInsets.all(0),
                       suffixIcon: Icon(
                         Icons.search,
                         color: Colors.black,
-                        size: 30,
+                        size: 25,
                       ),
-                      prefixIconConstraints:
-                          BoxConstraints(maxHeight: 20, minWidth: 25),
+                      suffixIconConstraints:
+                          BoxConstraints(maxHeight: 20.0, minWidth: 25.0),
                       border: InputBorder.none,
                       hintText: 'Search existing people in campus',
-                      hintStyle: TextStyle(color: Colors.grey, fontSize: 18)),
+                      hintStyle: TextStyle(color: Colors.grey)),
                 ),
               ),
+              const SizedBox(
+                height: 15,
+              ),
+              (newUserList.length == 0)
+                  ? Container(
+                      height: 200,
+                      width: 200,
+                      alignment: Alignment.center,
+                      child: Text(
+                        "No Person found",
+                        style: TextStyle(fontSize: 16),
+                      ))
+                  : Expanded(
+                      child: ListView.builder(
+                          padding: const EdgeInsets.all(0.0),
+                          itemCount: newUserList.length,
+                          shrinkWrap: true,
+                          itemBuilder: (BuildContext context, int index) {
+                            return ItemCard(
+                                countAdded: (p0) {
+                                  numOfPeopleAdded += p0;
+                                  setState(() {});
+                                },
+                                name: newUserList[index].name,
+                                imgurl: newUserList[index].imgurl);
+                          })),
             ],
           ),
         ),
+        floatingActionButton: SizedBox(
+          width: 154,
+          height: 40,
+          child: FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: const Color(0xFFFF0000),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Text(
+              'Create Group',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       );
 }
